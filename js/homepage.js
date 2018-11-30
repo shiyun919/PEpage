f03674a9a4c1f190c11edbc994e608f87922c879
duyu <c_duoyun@outlook.com>
2018-11-30 14:38:54 +0800

    commit

--- js/homepage.js
+++ js/homepage.js
@@ -16,8 +16,26 @@ $(function(){
         })
     });	
 });
-$(function(){
+
+//鼠标移动选中
+/*$(function(){
     $("#selected li").click(function(){
       $(this).addClass("now").siblings().removeClass("now");
     });
-  })
+  })*/
+
+
+//鼠标移动选中，下方的内容也跟着切换
+/*<script>
+    $(document).ready(function(){
+    // tab
+        var $tab_li = $('.tab_menu li');
+            $tab_li.hover(function(){
+            $(this).addClass('selected').siblings().removeClass('selected');
+            var index = $(this).index();
+            var childer = $(this).parents('.modular').find('.tab_info');
+            console.log()
+            childer.eq(index).show().siblings().hide();
+        });
+    });
+</script>*/
