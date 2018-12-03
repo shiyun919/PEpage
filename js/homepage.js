f602abf7c82d10e7812d9687099530bbc74aba67
duyu <c_duoyun@outlook.com>
2018-12-03 17:28:22 +0800

    commit

--- js/homepage.js
+++ js/homepage.js
@@ -15,6 +15,19 @@ $(function(){
             })
         })
     });	
+    $("div.pro-title").find("li").each(function () {
+        $(this).parent().parent().find("li").each(function (index, item) {
+            $(item).click(function () {
+                if (!$(item).hasClass("now")) {
+                    $(item).addClass("now");
+                    $(item).siblings("li").removeClass("now");
+                    var $showDiv = $(item).parent().parent().siblings("div:not(div.pro-title)").eq(index);
+                    $showDiv.show();
+                    $showDiv.siblings("div:not(div.pro-title)").hide();
+                }
+            })
+        })
+    });	
 });
 
 //鼠标移动选中
