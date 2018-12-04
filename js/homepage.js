632ccfbd3aded08c733cbcd34691b2c1d0b874d5
duyu <c_duoyun@outlook.com>
2018-12-04 14:22:42 +0800

    commit

--- js/homepage.js
+++ js/homepage.js
@@ -28,6 +28,19 @@ $(function(){
             })
         })
     });	
+    $("div.peKnowledge-t").find("li").each(function () {
+        $(this).parent().parent().find("li").each(function (index, item) {
+            $(item).click(function () {
+                if (!$(item).hasClass("now")) {
+                    $(item).addClass("now");
+                    $(item).siblings("li").removeClass("now");
+                    var $showDiv = $(item).parent().parent().siblings("div:not(div.peKnowledge-t)").eq(index);
+                    $showDiv.show();
+                    $showDiv.siblings("div:not(div.peKnowledge-t)").hide();
+                }
+            })
+        })
+    });	
 });
 
 //鼠标移动选中
